2fc6b91ef390da33383bcf51d3bac56c9e211cac
Nishit Pandit <nishitpandit@ymail.com>
2022-02-05 01:19:42 +0530

    Fixed vosk issues

--- JS/blob_to_mp3.js
+++ JS/blob_to_mp3.js
@@ -1,8 +1,8 @@
 var ffmpeg = require('ffmpeg')
 try{ 
-    var process = new ffmpeg('userSound')
+    var process = new ffmpeg('Sounds/user_sound_blob')
     process.then((audio)=>{
-    audio.fnExtractSoundToMP3("user.mp3", (error,file)=>{
+    audio.fnExtractSoundToMP3("Sounds/userSound.mp3", (error,file)=>{
       if(!error)
         console.log('Audio File: '+ file)
     })
