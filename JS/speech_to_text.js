fcc3e61cf413ab763d8c58d72fbbe1f7b194a795
Nishit Pandit <nishitpandit@ymail.com>
2022-02-11 16:20:13 +0530

    created a function to convert speech to text

--- JS/speech_to_text.js
+++ JS/speech_to_text.js
@@ -1,34 +1,36 @@
-var vosk = require('vosk')
+async function speechToText(){
 
-const fs = require("fs");
-const { spawn } = require("child_process");
+    var vosk = require('vosk')
+    const fs = require("fs");
+    const { spawn } = require("child_process")
+    const { once } = require('events')
 
-MODEL_PATH = "model"
-FILE_NAME = "sound.wav"
-SAMPLE_RATE = 16000
-BUFFER_SIZE = 4000
+    MODEL_PATH = "model"
+    FILE_NAME = "JS/Sounds/user_sound_blob"
+    SAMPLE_RATE = 16000
+    BUFFER_SIZE = 4000
 
-if (!fs.existsSync(MODEL_PATH)) {
-    console.log("Please download the model from https://alphacephei.com/vosk/models and unpack as " + MODEL_PATH + " in the current folder.")
-    process.exit()
+    if (!fs.existsSync(MODEL_PATH)) {
+        console.log("Please download the model from https://alphacephei.com/vosk/models and unpack as " + MODEL_PATH + " in the current folder.")
+        process.exit()
+    }
+
+    vosk.setLogLevel(-1);
+    const model = new vosk.Model(MODEL_PATH);
+    const rec = new vosk.Recognizer({model: model, sampleRate: SAMPLE_RATE});
+
+    const ffmpeg_run = spawn('ffmpeg', ['-loglevel', 'quiet', '-i', FILE_NAME,
+                            '-ar', String(SAMPLE_RATE) , '-ac', '1',
+                            '-f', 's16le', '-bufsize', String(BUFFER_SIZE) , '-']);
+
+    var output = ''
+    ffmpeg_run.stdout.on('data', (stdout) => {
+        rec.acceptWaveform(stdout)
+        output += (rec.finalResult()).text + ' '
+    });
+
+    await once(ffmpeg_run, 'close')
+    return output
 }
 
-if (process.argv.length > 2)
-    FILE_NAME = process.argv[2]
-
-vosk.setLogLevel(-1);
-const model = new vosk.Model(MODEL_PATH);
-const rec = new vosk.Recognizer({model: model, sampleRate: SAMPLE_RATE});
-
-const ffmpeg_run = spawn('ffmpeg', ['-loglevel', 'quiet', '-i', FILE_NAME,
-                         '-ar', String(SAMPLE_RATE) , '-ac', '1',
-                         '-f', 's16le', '-bufsize', String(BUFFER_SIZE) , '-']);
-
-ffmpeg_run.stdout.on('data', (stdout) => {
-    // if (rec.acceptWaveform(stdout))
-    //     console.log(rec.result());
-    // else
-    //     console.log(rec.partialResult());
-    rec.acceptWaveform(stdout)
-    console.log(rec.finalResult());
-});
+module.exports.speechToText = speechToText
